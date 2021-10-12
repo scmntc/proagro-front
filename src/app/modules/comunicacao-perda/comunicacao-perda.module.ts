e9a2f8fd74ce78bbba64f2b33d9fef70e51b3d34
Otavio <tavinq@hotmail.com>
2021-10-12 00:31:21 -0300

    Remoção translate

--- src/app/modules/comunicacao-perda/comunicacao-perda.module.ts
+++ src/app/modules/comunicacao-perda/comunicacao-perda.module.ts
@@ -1,11 +1,10 @@
-import {LOCALE_ID, NgModule} from '@angular/core';
+import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ComunicacaoPerdaComponent} from './comunicacao-perda.component';
 import {RouterModule, Routes} from "@angular/router";
 import {CardModule} from "primeng/card";
 import {AutoCompleteModule} from "primeng/autocomplete";
 import {ScrollingModule} from "@angular/cdk/scrolling";
-import {AutocompleteEditModule} from "../autocomplete-edit/autocomplete-edit.module";
 import {ComunicacaoPerdaListaComponent} from './comunicacao-perda-lista/comunicacao-perda-lista.component';
 import {TableModule} from "primeng/table";
 import {ProgressBarModule} from "primeng/progressbar";
@@ -15,7 +14,6 @@ import {ButtonModule} from "primeng/button";
 import {ToastModule} from "primeng/toast";
 import {InputTextModule} from "primeng/inputtext";
 import {CalendarModule} from "primeng/calendar";
-import {TranslateModule} from "@ngx-translate/core";
 import {AccordionModule} from "primeng/accordion";
 import {FieldsetModule} from "primeng/fieldset";
 import {DropdownModule} from "primeng/dropdown";
@@ -51,7 +49,6 @@ const routes: Routes = [
         ReactiveFormsModule,
         InputTextModule,
         CalendarModule,
-        TranslateModule.forChild(),
         AccordionModule,
         FieldsetModule,
         DropdownModule,
