28d082ac722b682b9d2daba4941ca8b2ca7bc268
Otavio <tavinq@hotmail.com>
2021-10-13 12:35:22 -0300

    heroku env cfg

--- src/app/services/heroku-env/heroku-env.service.ts
+++ src/app/services/heroku-env/heroku-env.service.ts
@@ -9,14 +9,15 @@ export class HerokuEnvService {
 
   constructor(private http: HttpClient) { }
 
+  // ia utilizar para implementar o google maps.
   getEnvironmentVariablesHeroku() {
-    this.http.get("./../assets/heroku-env.json").subscribe((data: any) => {
+   /* this.http.get("./../assets/heroku-env.json").subscribe((data: any) => {
       if (data.mapsAPI) {
         environment.mapsAPI = data.mapsAPI;
       }
       console.log(environment, data);
     }, error => {
       console.log(error);
-    })
+    })*/
   }
 }
